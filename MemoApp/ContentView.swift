41b47ce2f3a5a524657baac269bc861360ae7a05
110w110 <110w110.official@gmail.com>
2024-09-18 16:47:50 +0900

    [프로젝트 생성] [수정내용] -  없음
    [영향도]
    - 없음

---
+++ MemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoApp
+//
+//  Created by 한태희 on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by 한태희 on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
