06e86c575f1d832445a3aaea57c4cdeb54dbdce4
110w110 <110w110.official@gmail.com>
2024-09-19 14:30:10 +0900

    [MainView 내부 UI 구현] [수정내용] - 네비게이션 위 아래 버튼 제외 UI 구현 - 세부 UI는 ViewBuilder를 통해 구분하여 구현
    [영향도]
    - MainView

--- MemoApp/Main/MainView.swift
+++ MemoApp/Main/MainView.swift
@@ -13,7 +13,83 @@ struct MainView: View {
   
   var body: some View {
     WithViewStore(store, observe: { $0 }) { viewStore in
-      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView {
+        ScrollView {
+          searchBar()
+          section(header: "나의 iPhone")
+        }
+        .background(
+          Color(uiColor: .groupTableViewBackground)
+            .edgesIgnoringSafeArea(.all)
+        )
+        .navigationTitle("폴더")
+        .navigationBarTitleDisplayMode(.large)
+      }
     }
+  }
+  
+  @ViewBuilder private func searchBar() -> some View {
+    ZStack {
+      RoundedRectangle(cornerRadius: 10)
+        .foregroundColor(Color(uiColor: .systemGray5))
+      HStack {
+        Image(systemName: "magnifyingglass")
+        Text("검색")
+        Spacer()
+        Image(systemName: "mic.fill")
+      }
+      .foregroundColor(Color(uiColor: .gray))
+      .padding(.horizontal, 5)
+    }
+    .frame(height: 36)
+    .padding(.horizontal, 20)
+    .padding(.vertical, 5)
+  }
+  
+  @ViewBuilder private func section(header: String = "") -> some View {
+    WithViewStore(store, observe: \.folderList) { folderList in
+      VStack {
+        HStack {
+          Text(header)
+            .font(.title3)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+          Spacer()
+        }
+        ZStack {
+          RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(Color(uiColor: .systemBackground))
+          VStack(spacing: 0) {
+            ForEach(folderList.indices, id: \.self) { index in
+              row(folder: folderList[index])
+              if index < folderList.count - 1 {
+                Divider()
+              }
+            }
+          }
+        }
+      }
+      .padding(.horizontal, 20)
+      .padding(.vertical, 5)
+    }
+  }
+  
+  @ViewBuilder private func row(folder: Folder) -> some View {
+    HStack {
+      Image(systemName: folder.iconName)
+        .resizable()
+        .foregroundColor(.yellow)
+        .frame(width: 24, height: 20)
+      Text(folder.title)
+        .padding(.horizontal, 10)
+      Spacer()
+      Text("\(folder.memoList.count)")
+        .foregroundColor(.gray)
+      Image(systemName: "chevron.right")
+        .foregroundColor(.gray)
+    }
+    .frame(height: 40)
+    .padding(.horizontal, 20)
+    .padding(.vertical, 5)
   }
 }
